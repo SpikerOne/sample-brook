aa75f0e3d66fbb124fe4a858bffbd0d1a9a1bc00
SpikerOne <96699983+SpikerOne@users.noreply.github.com>
2022-10-01 13:44:51 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 ## Install brook
 wget -q https://github.com/txthinking/brook/releases/latest/download/brook_linux_amd64 -O /usr/local/bin/brook
 chmod +x /usr/local/bin/brook
+tail -f /dev/null
 
 ## Start service
 ## brook wsserver --listen :${PORT} --password ${passwd}
